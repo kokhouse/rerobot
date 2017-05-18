dde4c1111f77250fbda0fff6ddb7556cb083949c
luxory <dovescrymgmt@gmail.com>
2017-05-18 11:21:58 -0400

    setup botto

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Rero',
+    version='0.0.1-beta-1',
+    packages=['test', 'Rero', 'Rero.Modules'],
+    url='http://rero.xyz/',
+    license='Apache License, Version 2.0',
+    author='Luxory',
+    author_email='dovescrymgmt@gmail.com',
+    description='A multipurpose bot for Discord'
+)
