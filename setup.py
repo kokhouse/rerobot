7bbcdc8a6eaf6e1e98aa3e26535e352f0380b5c5
luxory <dovescrymgmt@gmail.com>
2017-05-18 11:26:51 -0400

    config

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='Rero',
     version='0.0.1-beta-1',
     packages=['test', 'Rero', 'Rero.Modules'],
-    url='http://rero.xyz/',
+    url='',
     license='Apache License, Version 2.0',
     author='Luxory',
     author_email='dovescrymgmt@gmail.com',
